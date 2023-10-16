2c1474da8537c93a34956ba1af12901abe0e6b3b
Anthea Ip <antheaip964@gmail.com>
2023-10-16 18:30:47 -0400

    Saving

---
+++ src/main/java/com/example/ToDoApp/ToDoService.java
@@ -0,0 +1,30 @@
+package com.example.ToDoApp;
+import com.example.ToDoApp.model.ToDo;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+public class ToDoService ToDoService {
+
+    List<ToDo> getAllToDoItems() {
+
+    }
+
+    ToDo getToDoItemById() {
+
+    }
+
+    boolean updateStatus() {
+
+    }
+
+
+    boolean saveOrUpdateToDoItem() {
+
+    }
+
+    boolean deleteToDoItem() {
+
+    }
+
+
+}
--- src/main/java/com/example/ToDoApp/model/ToDo.java
+++ src/main/java/com/example/ToDoApp/model/ToDo.java
@@ -2,28 +2,62 @@ package com.example.ToDoApp.model;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
-import org.hibernate.annotations.Columns;
+
 import java.util.Date;
 @Entity
-@Table(name="ToDo")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-
+@Table (name="todo")
 
 public class ToDo {
-    @Id@GeneratedValue(strategy = GenerationType.AUTO);
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Columns
+    @Column
     private String title;
-    @Columns
+    @Column
     private Date date;
-    @Columns
+    @Column
     private String status;
+
+    // Constructor
+    public ToDo() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
