d98550eaafd8ffafa7bf82f717fc4f25c6743a16
Anthea Ip <antheaip964@gmail.com>
2023-10-16 18:46:25 -0400

    Saving

--- src/main/java/com/example/ToDoApp/repo/IToDoRepo.java
+++ src/main/java/com/example/ToDoApp/repo/IToDoRepo.java
@@ -1,7 +1,9 @@
-package com.example.ToDoApp;
+package com.example.ToDoApp.repo;
 
 import com.example.ToDoApp.model.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IToDoRepo extends JpaRepository<ToDo, Long> {
 }
